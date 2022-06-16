1369e2e7de1ec1aaf068331109e946f96d5d7b39
Ken <kdjiang390@gmail.com>
2022-06-15 20:29:50 -0500

    trying again

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./setup.sh && streamlit run Home.py
+web: sh setup.sh && streamlit run Home.py
--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
 mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"kdjiang390@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
-\n\
 " > ~/.streamlit/config.toml
